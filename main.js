4d51cb38b969433c3bd9149f6af15b95333ae059
Chin <a.chin.chen@hotmail.com>
2018-10-09 18:21:57 +0800

    1. modify style in main.sass 2. add class of Cards in main.js

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 class Cards {
-  constructor(value) {
+  constructor() {
     this.colors = [ 'blue', 'green', 'pink', 'yellow' ]
-    this.shapes = ['diamond', 'oval', 'rectangle', 'triangle']
-    this.allOn = false
-    this.allCards = document.querySelectorAll('.card-back')
-    this.setCards = () => {
-      this.allCards.forEach(card => {
-        // card.style.backgroundImage = 
+    this.shapes = [ 'diamond', 'oval', 'rectangle', 'triangle' ]
+  }
+  get cardList() {
+    return this.getCardList()
+  }
+  getCardList() {
+    let list = []
+    this.colors.forEach(color => {
+      this.shapes.forEach(shape => {
+        list.push({ cardImage: `${shape}-${color}.svg`, color, shape })
       })
-    }
+    })
+    return list
   }
 }
-var cards = new Cards()
+
+class Game {
+  constructor() {
+    this.cards = new Cards()
+    this.allCards = document.querySelectorAll('.card-back')
+  }
+  get levels() {
+    return this.getLevels()
+  }
+  getLevels() {
+    let randomLevels = []
+    
+  }
+}
+
+let game = new Game()
