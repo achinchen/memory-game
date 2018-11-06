09c6776ef3b04a414a57f47d7ee969c9a07b66e4
Chin <a.chin.chen@hotmail.com>
2018-11-06 18:48:23 +0800

    add webpack.config.js

--- README.md
+++
@@ -1,5 +0,0 @@
-Memory Game
-===
-### This a plain Game!
-#### Make a memory game for implementing object-oriented design in JS!
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path')
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  mode: 'production',
+  entry: [
+    './src/main.js',
+    './src/style/main.sass'
+  ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+      // path: path.resolve(__dirname, 'dist')
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer()
+        ]
+      }
+    })
+  ]
 }
